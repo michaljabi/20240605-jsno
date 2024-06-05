729496bd147d1994efdb677796608755f2bbfbd9
Jabi <michal.jabi@gmail.com>
2024-06-05 11:54:37 +0200

    explain(l001): why not using var in code.

---
+++ JSNO-teoria/L-abolatory/L001-why-no-var-in-code.js
@@ -0,0 +1,27 @@
+// po 2015:
+
+{
+    let x = 10;
+    x = 12;
+    console.log(x);
+}
+
+// console.log(x);
+
+{
+    const x = 10;
+    // x = 12;
+    console.log(x);
+}
+
+// console.log(x);
+
+{
+    var x = 200;
+    console.log(x);
+}
+
+// POPSUTE od samego początku:
+// zadziała dobrze tylko w function !
+console.log(x);
+
