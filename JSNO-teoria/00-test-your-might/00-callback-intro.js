a4926fdfbfe9d21c28e42ed4c639f109e092aa7e
Jabi <michal.jabi@gmail.com>
2024-06-05 10:38:39 +0200

    example(00): callbacks solved.

--- JSNO-teoria/00-test-your-might/00-callback-intro.js
+++ JSNO-teoria/00-test-your-might/00-callback-intro.js
@@ -1,8 +1,13 @@
 
 console.log('Hello stranger... shall we start?')
 
+// PROVIDER:
 function giveMeTheNumber(mistery) {
-    mistery(300)
+    // mistery(300)
+    mistery(10, 'a')
+    // setTimeout(() => {
+    //     mistery(6000)
+    // }, 5000);
 }
 
 /**
@@ -14,3 +19,31 @@ function giveMeTheNumber(mistery) {
 // w terminalu:
 // node ścieżka/do/pliku.js
 
+// // function expression
+// giveMeTheNumber(number => {
+//     console.log(number)
+// })
+
+// CONSUMER 1:
+
+// // function expression
+// giveMeTheNumber(function (n) {
+//     console.log(n)
+//     n
+// })
+
+
+// CONSUMER 2:
+
+// function definition
+function recieveTheNumber(no, letter) {
+    console.log(no)
+    console.log(letter)
+}
+
+// giveMeTheNumber(recieveTheNumber)
+
+giveMeTheNumber((a, b) => console.log(a,b));
+
+// ...rest parameter (po 2015)
+giveMeTheNumber((...arg) => console.log(arg));
