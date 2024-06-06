97e619d492f935fae5d0e97bb6af3a93acf173b3
Jabi <michal.jabi@gmail.com>
2024-06-06 14:10:26 +0200

    feat(task-6): make it work - events!

---
+++ JSNO-zadania/6-using-events-to-simplify-the-flow/src/main.js
@@ -0,0 +1,52 @@
+import { EventEmitter } from 'node:events'
+import { createInterface } from 'node:readline';
+import { stdin, stdout, exit } from 'node:process';
+
+const programEventBus = new EventEmitter();
+const readline = createInterface(stdin, stdout)
+
+console.log('[Path Segments 1.0]');
+
+programEventBus.on('onStart', () => {
+    console.log('Proszę czekać, ładowanie danych...')
+    setTimeout(() => {
+        programEventBus.emit('onLoad')
+    }, 3000)
+})
+
+programEventBus.on('onLoad', () => {
+    console.log(`----
+Witaj.
+Obsługa programu: 
+  1. podaj ścieżkę
+  lub
+  2. wpisz: koniec - aby zakończyć działanie programu`)
+    programEventBus.emit('onPathSegmentsRequested')
+})
+
+programEventBus.on('onPathSegmentsRequested', () => {
+    readline.question('Podaj ścieżkę: ', (value) => {
+        if (value === 'koniec') {
+            programEventBus.emit('onClose');
+        } else {
+            const segments = value.split(/[\/\\]/);
+            console.log(`
+- Liczba segmentów ${segments.length}
+- Segmenty: ${segments}`)
+            programEventBus.emit('onPathSegmentsRequested')
+        }
+    })
+})
+
+programEventBus.on('onClose', () => {
+    console.log(`
+---
+Żegnaj.
+`)
+    // readline.close();
+    exit();
+})
+
+programEventBus.emit('onStart');
+
+
