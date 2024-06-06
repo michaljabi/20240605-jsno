898b123367cddfc195086a1137c208efbbfcaeec
Jabi <michal.jabi@gmail.com>
2024-06-06 11:38:26 +0200

    explain(L002): EventLoop / order of execution

---
+++ JSNO-teoria/L-abolatory/L002-order-of-execution-event-loop.js
@@ -0,0 +1,21 @@
+// task
+setTimeout(() => {
+    console.log('1')
+})
+
+console.log('2')
+
+// micro-task
+Promise.resolve('3').then((value) => {
+    console.log(value)
+})
+
+// Event loop:
+// 1 proste zadanie ma.
+
+// TODO list:
+// 1. sprawdź czy stos wywołania jest pusty:
+// 2. jeśli tak, sprawdź czy coś jest w kolejce
+    // 2.1 najpierw sprawdzę kolejkę micro-task
+    // 2.2 potem sprawdzę kolejkę task
+// 3. jeśli jest w kolejce - wrzuć na stos wywołania.
