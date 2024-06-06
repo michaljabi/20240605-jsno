3ab66f252b71e890b6eec72f499a10098a61914d
Jabi <michal.jabi@gmail.com>
2024-06-06 11:38:40 +0200

    explain(L002): EventLoop / yt video

--- JSNO-teoria/L-abolatory/L002-order-of-execution-event-loop.js
+++ JSNO-teoria/L-abolatory/L002-order-of-execution-event-loop.js
@@ -19,3 +19,7 @@ Promise.resolve('3').then((value) => {
     // 2.1 najpierw sprawdzę kolejkę micro-task
     // 2.2 potem sprawdzę kolejkę task
 // 3. jeśli jest w kolejce - wrzuć na stos wywołania.
+
+
+// Jak to działa pod spodem:
+// https://www.youtube.com/watch?v=8aGhZQkoFbQ
