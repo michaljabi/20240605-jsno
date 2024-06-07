f173b329baeb34baeb8f32783f675f40bf4bb0b2
Jabi <michal.jabi@gmail.com>
2024-06-07 14:33:24 +0200

    refactor(task-91): ServerError

--- JSNO-zadania/91-simple-express-js-server/src/main.js
+++ JSNO-zadania/91-simple-express-js-server/src/main.js
@@ -5,6 +5,7 @@ import express from 'express'
 
 import { dataController } from './data/data.controller.js';
 import { usersController } from './users/users.controller.js';
+import { ServerError } from './ServerError.js';
 
 
 const PORT = Number(process.env.PORT) || 3000;
@@ -32,7 +33,7 @@ server.use('/users', usersController)
 
 
 server.all('**', (req, res) => {
-    res.json({ error: `404 - nie znam ścieżki ${req.url}` });
+    res.status(404).json({ error: `404 - nie znam ścieżki ${req.url}` });
 })
 
 // Error handling middleware:
@@ -41,8 +42,17 @@ server.use((err, req, res, next) => {
 
     console.error(err);
 
-    res.status(500).json({
+    const status = err instanceof ServerError ? err.status : 500;
+
+    // if (err instanceof ServerError) {
+    //     res.status(err.status)
+    // } else {
+    //     res.status(500)
+    // }
+
+    res.status(status).json({
         error: 'An error occured',
+        status,
         message: err.message
     })
 })
--- JSNO-zadania/91-simple-express-js-server/src/users/users.service.js
+++ JSNO-zadania/91-simple-express-js-server/src/users/users.service.js
@@ -1,5 +1,10 @@
 import { nanoid } from 'nanoid'
+import { EventEmitter } from 'node:events'
 
+import { ServerError } from '../ServerError.js';
+
+
+export const userEvents = new EventEmitter()
 
 const users = [];
 let id = 1;
@@ -16,6 +21,7 @@ export const usersService = {
             ...userDto
         }
         users.push(user)
+        userEvents.emit('new-user-inserted', user);
         return user;
     },
     async getAll(name = '') {
@@ -24,7 +30,7 @@ export const usersService = {
     async getByUuid(uuid) {
         const user = users.find(u => u.uuid === uuid);
         if (!user) {
-            throw new Error(`User with uuid ${uuid} not found`)
+            throw new ServerError(`User with uuid ${uuid} not found`, 404)
         }
         return user;
     },
